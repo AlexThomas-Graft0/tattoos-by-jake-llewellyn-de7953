c5dadf538ad0d56e0a09e58e12d05756aece5902
Alex Thomas <48531362+AlexThomas-Graft0@users.noreply.github.com>
2026-07-07 12:55:43 +0100

    add app/sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,5 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [{ url: "https://tattoos-by-jake-llewellyn-de7953.duckbyte.co" }];
+}
