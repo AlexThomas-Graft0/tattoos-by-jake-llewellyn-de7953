17f0a6076e19f2e261b3e4afdf78122009e83d8b
Alex Thomas <48531362+AlexThomas-Graft0@users.noreply.github.com>
2026-07-07 12:55:41 +0100

    add lib/supabaseClient.ts

---
+++ lib/supabaseClient.ts
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+  { db: { schema: "tattoos_by_jake_llewellyn_de7953" } },
+);
